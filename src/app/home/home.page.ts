5bc1da22aea53acd6618c2203bcd630bd4ab0b20
Gabriel e Vitão <gabriel.sgo@hotmail.com>
2022-09-27 20:32:29 -0300

    projeto quase finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  qtdAdultos: number  ;
+  qtdCriancas: number  ;
+  totalSalgadinho: number  ;
+  totalRefrigerante: number  ;
+  totalBolo: number   ;
 
-  constructor() {}
+  constructor() { }
 
+  calcular() {
+    this.totalSalgadinho = this.qtdAdultos * 10 + this.qtdCriancas * 7;
+    this.totalRefrigerante = (this.qtdAdultos * 400 + this.qtdCriancas * 300)/1000;
+    this.totalBolo = (this.qtdAdultos *100+ this.qtdCriancas*100)/1000;
+
+  }
+  limpar(){
+    this.totalSalgadinho = 0;
+    this.totalRefrigerante = 0;
+    this.totalBolo = 0;
+    this.calcular();
+  }
+
+  
 }
